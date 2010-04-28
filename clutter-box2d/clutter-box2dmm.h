53fcd4ef418c7b220969e5bd9006e90713d99a4b
David King <davidk@openismus.com>
2010-04-28 16:58:06 +0200

    Add main page to Doxygen documentation
    * doc/Makefile.am: Parse clutter-box2d/clutter-box2dmm.h for
    documentation.
    * clutter-box2d/clutter-box2dmm.h: Add main page to Doxygen
    documentation.

--- clutter-box2d/clutter-box2dmm.h
+++ clutter-box2d/clutter-box2dmm.h
@@ -20,6 +20,39 @@
 #ifndef LIBCLUTTER_BOX2DMM_H
 #define LIBCLUTTER_BOX2DMM_H
 
+/** @mainpage clutter-box2dmm Reference Manual
+ *
+ * @section description Description
+ *
+ * clutter-box2dmm is a C++ wrapper for clutter-box2d.
+ *
+ * See also the <a href="http://www.clutter-project.org/">clutter website</a>.
+ *
+ * @section basics Basic Usage
+ * Include the clutter-box2dmm header:
+ * @code
+ * #include <clutter-box2dmm.h>
+ * @endcode
+ * (You may include individual headers, such as @c clutter-box2dmm/box2d.h
+ * instead.)
+ *
+ * If your source file is @c program.cc, you can compile it with:
+ * @code
+ * g++ program.cc -o program `pkg-config --cflags --libs clutter-box2dmm-0.10`
+ * @endcode
+ *
+ * Alternatively, if using autoconf, use the following in @c configure.ac:
+ * @code
+ * PKG_CHECK_MODULES([CLUTTER_BOX2DMM], [clutter-box2dmm-0.10])
+ * @endcode
+ * Then use the generated @c CLUTTER_BOX2DMM_CFLAGS and @c CLUTTER_BOX2DMM_LIBS
+ * variables in the project @c Makefile.am files. For example:
+ * @code
+ * program_CPPFLAGS = $(CLUTTER_BOX2DMM_CFLAGS)
+ * program_LDADD = $(CLUTTER_BOX2DMM_LIBS}
+ * @endcode
+ */
+
 /* cluttermm version.  */
 extern const int clutter_box2dmm_major_version;
 extern const int clutter_box2dmm_minor_version;
